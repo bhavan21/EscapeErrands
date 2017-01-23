c0f6605deeb5bc8d19b45d07ec67ad771f927f3a
Yashasvi-Sriram <yash.3997@gmail.com>
2017-01-23 16:27:07 +0530

    adds css and rearranges files for the sake of modularity
    1.adds css : errands/touch.html and time_table/scrolling_stubs.html
    2.adds icons (web and mobile)
    3.moves base.js moment.js highcharts.js to home app from errands app
    4.creates : a python file colors.py in time_table app
            and moves the def color_map(pk) from errands.models to time_table.colors

--- errands/models.py
+++ errands/models.py
@@ -2,30 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 from datetime import datetime as dt, timedelta as td
+from time_table.colors import color_map
 import json
-
-
-def color_map(pk):
-    if pk % 10 == 0:
-        return '#01579b'
-    elif pk % 10 == 1:
-        return '#e65100'
-    elif pk % 10 == 2:
-        return '#2e7d32'
-    elif pk % 10 == 3:
-        return '#6200ea'
-    elif pk % 10 == 4:
-        return '#4e342e'
-    elif pk % 10 == 5:
-        return '#827717'
-    elif pk % 10 == 6:
-        return '#455a64'
-    elif pk % 10 == 7:
-        return '#f50057'
-    elif pk % 10 == 8:
-        return '#ffd600'
-    elif pk % 10 == 9:
-        return '#212121'
 
 
 class Std:
---
+++ time_table/colors.py
@@ -0,0 +1,21 @@
+def color_map(pk):
+    if pk % 10 == 0:
+        return '#01579b'
+    elif pk % 10 == 1:
+        return '#e65100'
+    elif pk % 10 == 2:
+        return '#2e7d32'
+    elif pk % 10 == 3:
+        return '#6200ea'
+    elif pk % 10 == 4:
+        return '#4e342e'
+    elif pk % 10 == 5:
+        return '#827717'
+    elif pk % 10 == 6:
+        return '#455a64'
+    elif pk % 10 == 7:
+        return '#f50057'
+    elif pk % 10 == 8:
+        return '#e65100'
+    elif pk % 10 == 9:
+        return '#212121'

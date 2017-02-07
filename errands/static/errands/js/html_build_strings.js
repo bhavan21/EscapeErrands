c20ca44b514c78bbd7a6d3fc512bfbd7981160ee
Yashasvi-Sriram <yash.3997@gmail.com>
2017-02-07 20:18:41 +0530

    In errands/touch.html 	1.implements form for template 	2.adds exam template 	3.improves the new_piece_add() function
    In errands/html_build_strings.js
    	1.Optimizes interface for mobile devices
    	2.Improves CSS

--- errands/static/errands/js/html_build_strings.js
+++ errands/static/errands/js/html_build_strings.js
@@ -28,7 +28,6 @@ var HTML_STRINGS = {
                 time_period_display = 'display:none';
                 time_period_switch = '';
             }
-
             return '<tr id="' + id + '">' +
                 '<td class="center-align remove-td">' +
                 '<a class="remove-btn waves-effect waves-light btn-floating red z-depth-3">' +
@@ -37,9 +36,9 @@ var HTML_STRINGS = {
                 '</td>' +
                 '<td>' +
                 '<div class="card waves-effect waves-yellow hoverable" style="width:100%">' +
-                '<span><i class="small material-icons activator right waves-effect">comment</i></span>' +
+                '<span style="position:absolute;bottom:0;right:0;margin:10px"><i class="material-icons activator">comment</i></span>' +
                 '<div class="card-content row">' +
-                '<div class="col l6 m6 s6 waves-effect waves-yellow  time-period-div">' +
+                '<div class="col l6 m6 s12 waves-effect waves-yellow  time-period-div">' +
                 '<div class="card-title">Time Period</div>' +
                 '<div class="row">' +
                 '<div class="col l4 m4 s4 switch">' +
@@ -62,7 +61,7 @@ var HTML_STRINGS = {
                 '</div>' +
                 '</div>' +
                 '</div>' +
-                '<div class="col l6 m6 s6 waves-effect waves-yellow duration-div">' +
+                '<div class="col l6 m6 s12 waves-effect waves-yellow duration-div">' +
                 '<div class="card-title">Duration</div>' +
                 '<div class="row">' +
                 '<div class="col l4 m4 s4 switch">' +
@@ -87,21 +86,21 @@ var HTML_STRINGS = {
                 '</div>' +
                 '</div>' +
                 '<div class="card-content row">' +
-                '<div class="col l8 m8 s8 waves-effect waves-yellow epoch-div">' +
+                '<div class="col l8 m8 s12 waves-effect waves-yellow epoch-div">' +
                 '<div class="card-title">Epoch</div>' +
                 '<div class="row">' +
-                '<label class="col l6 m6 s6 ">' +
+                '<label class="col l6 m6 s12 ">' +
                 'Epoch Date' +
                 '<input class="epoch-date" type="date" value="' + epoch_date + '">' +
                 '</label>' +
                 '<div class="col l1 m1 s1"></div>' +
-                '<label class="col l6 m6 s6 ">' +
+                '<label class="col l6 m6 s12 ">' +
                 'Epoch Time' +
                 '<input class="epoch-time" type="time" value="' + epoch_time + '">' +
                 '</label>' +
                 '</div>' +
                 '</div>' +
-                '<div class="col l3 m3 s3 waves-effect waves-yellow tag-div">' +
+                '<div class="col l4 m4 s12 waves-effect waves-yellow tag-div">' +
                 '<div class="card-title">About</div>' +
                 '<label>' +
                 'Tag' +
@@ -110,15 +109,14 @@ var HTML_STRINGS = {
                 '</div>' +
                 '</div>' +
                 '<div class="card-content row">' +
-                '<div class="col l8 m8 s8 waves-effect waves-yellow end-div" style="' + time_period_display + '">' +
+                '<div class="col l8 m8 s12 waves-effect waves-yellow end-div" style="' + time_period_display + '">' +
                 '<div class="card-title">End</div>' +
                 '<div class="row">' +
-                '<label class="col l6 m6 s6 ">' +
+                '<label class="col l6 m6 s12 ">' +
                 'End Date' +
                 '<input class="end-date" type="date" value="' + end_date + '">' +
                 '</label>' +
-                '<div class="col l1 m1 s1"></div>' +
-                '<label class="col l6 m6 s6 ">' +
+                '<label class="col l6 m6 s12 ">' +
                 'End Time' +
                 '<input class="end-time" type="time" value="' + end_time + '">' +
                 '</label>' +
@@ -126,22 +124,26 @@ var HTML_STRINGS = {
                 '</div>' +
                 '</div>' +
                 '<div class="card-reveal">' +
-                '<span class="card-title grey-text text-darken-4">' +
-                '<i class="material-icons right waves-effect">close</i></span>' +
+                '<span style="position:absolute;bottom:0;right:0;margin:10px" class="card-title">' +
+                '<i class="material-icons waves-effect">close</i></span>' +
                 '<div class="card-title">Comment</div>' +
                 '<div class="input-field">' +
                 '<textarea class="comment materialize-textarea">' + comment + '</textarea>' +
                 '</div>' +
-                'Help' +
+                '<div class="grey-text">' +
+                '<span class="flow-text">Help</span>' +
                 '<ul>' +
-                '<li>An Event has Duration, A Task Do Not<li>' +
+                '<li>An Event has duration, A Task does not<li>' +
                 '<li>Both can repeat<li>' +
-                '<li>For those which repeat, Epoch Date is it\'s First Date<li>' +
+                '<li>For those which repeat, their first date is Epoch DateTime and they cannot appear after End DateTime<li>' +
+                '<li>Using these thumb rules, construct your errand in an intuitional manner<li>' +
                 '</ul>' +
+                '</div>' +
                 '</div>' +
                 '</div>' +
                 '</td>' +
                 '</tr>'
+
         }
     },
     ALL: {

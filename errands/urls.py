c9fb3bf98b88656b81131e6873f9fda133fa917d
Yashasvi-Sriram <yash.3997@gmail.com>
2017-02-20 20:16:59 +0530

    futher renames (read) urls
    fixes : bug in errands/all.html sync pieces btn
    	... previously if sync failed the sync btn would not appear back
    	... runs a infinite preloader loop
    	... now, in case of error comes to initial state

--- errands/urls.py
+++ errands/urls.py
@@ -13,12 +13,12 @@ urlpatterns = [
     url(r'^process_touch/errand/$', views.process_touch, name='process_touch_errand'),
     # host/errands/delete/errand/ -- pk is sent through POST method for security
     url(r'^delete/errand/$', views.delete_errand, name='delete_errand'),
-    # host/errands/read_errand/pk/
-    url(r'^read_errand/(?P<pk>[0-9]+)/$', views.read_errand, name='read_errand'),
+    # host/errands/read/errand/pk/
+    url(r'^read/errand/(?P<pk>[0-9]+)/$', views.read_errand, name='read_errand'),
 
-    # host/errands/read_piece/pk/
-    url(r'^read_piece/(?P<pk>[0-9]+)/$', views.read_piece, name='read_piece'),
+    # host/errands/read/piece/pk/
+    url(r'^read/piece/(?P<pk>[0-9]+)/$', views.read_piece, name='read_piece'),
 
-    # host/errands/read_stubs/
-    url(r'^read_stubs/$', views.read_stubs, name='read_stubs'),
+    # host/errands/read/stubs/
+    url(r'^read/stubs/$', views.read_stubs, name='read_stubs'),
 ]

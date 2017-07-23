a37f601e52d1a2655f36c2be6a053468e473d86b
Yashasvi-Sriram <yash.3997@gmail.com>
2017-07-24 00:26:37 +0530

    adds: logo, materialize css js
    implements:
    	1.base.html for webapi app
    	2.goal_crud.html for handling goals
    		...can search among goals from descriptions using regex
    		...can render graph of family of a goal

--- webapi/urls.py
+++ webapi/urls.py
@@ -4,6 +4,10 @@ from . import views
 app_name = 'webapi'
 
 urlpatterns = [
-    # host/webapi/goal/gli/
-    url(r'^goal/gli/$', views.GoalCRUD.list, name='goal_gli'),
+    # host/webapi/goal/crud/
+    url(r'^goal/crud/$', views.crud, name='goal_crud'),
+    # host/webapi/goal/read/regex/<GET PARAM>
+    url(r'^goal/read/regex/$', views.read_regex, name='goal_read_regex'),
+    # host/webapi/goal/read/family/pk
+    url(r'^goal/read/family/(?P<pk>[1-9][0-9]*)/$', views.read_family, name='goal_read_family'),
 ]
--- webapi/views.py
+++ webapi/views.py
@@ -1,13 +1,64 @@
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse
 from django.shortcuts import render
-
+import re
+import json
 from core.models.goal import Goal
 
 
-class GoalCRUD:
-    def __init__(self):
-        pass
+def crud(request):
+    return render(request, 'webapi/goal_crud.html')
 
-    @staticmethod
-    def list(request):
-        goal_list = Goal.objects.all()
-        return render(request, 'webapi/goal/gli.html', {'goal_list': goal_list})
+
+def to_json_style(goal):
+    deadline = None
+    if goal.deadline is not None:
+        deadline = goal.deadline.strftime('%c')
+    parent_ids = []
+    for parent in goal.get_parents():
+        parent_ids.append(parent.id)
+    child_ids = []
+    for child in goal.get_children():
+        child_ids.append(child.id)
+
+    json_goal = {
+        'description': goal.description,
+        'deadline': deadline,
+        'is_achieved': goal.is_achieved,
+        'id': goal.id,
+        'parent_ids': parent_ids,
+        'child_ids': child_ids
+    }
+    return json_goal
+
+
+def read_regex(request):
+    if request.method == 'GET' and 'search' in request.GET:
+        pattern = request.GET['search']
+        try:
+            re.compile(pattern)
+        except re.error:
+            return HttpResponse(json.dumps({'status': -1, 'message': 'Not proper regex'}))
+        matched_goals = Goal.objects.filter(description__iregex=pattern)
+        json_goals = []
+        for goal in matched_goals:
+            json_goals.append(to_json_style(goal))
+
+        return HttpResponse(json.dumps({'status': 0, 'body': json_goals}))
+    else:
+        return HttpResponse(json.dumps({'status': -1, 'message': 'No search string in request'}))
+
+
+def read_family(request, pk):
+    try:
+        goal = Goal.objects.get(pk=pk)
+        family_of_ids = goal.get_family_set()
+        json_family = []
+        for member_id in family_of_ids:
+            member = Goal.objects.get(pk=member_id)
+            json_family.append(to_json_style(member))
+
+        return HttpResponse(json.dumps({'status': 0, 'body': json_family}))
+
+    except ObjectDoesNotExist:
+        return HttpResponse(json.dumps({'status': -1, 'message': 'No goal with such id'}))

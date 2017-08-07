e43e87783a4a8fc8d864aeb0d0225a20479691fc
Yashasvi-Sriram <yash.3997@gmail.com>
2017-08-07 19:51:24 +0530

    adds: homepage
    improves: todos

--- webinterface/urls.py
+++ webinterface/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'webinterface'
 
 urlpatterns = [
+    # host/
+    url(r'^$', views.home, name='home'),
     # host/goal/glance/
     url(r'^goal/glance/$', views.goal_glance, name='goal_glance'),
 ]

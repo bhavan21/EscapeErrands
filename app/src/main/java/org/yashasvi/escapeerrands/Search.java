b3642b0108bfcae2016f473173a41e80e66042b9
Yashasvi-Sriram <yash.3997@gmail.com>
2017-08-19 21:57:23 +0530

    adds: retrofit lib as dependency
    adds: INTERNET permission in manifest
    
    implements: menu in Search activity and a search view and its listeners

--- app/src/main/java/org/yashasvi/escapeerrands/Search.java
+++ app/src/main/java/org/yashasvi/escapeerrands/Search.java
@@ -1,11 +1,32 @@
 package org.yashasvi.escapeerrands;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
 
 import org.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.a_search)
 public class Search extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_menu_search_item).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String string) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String string) {
+                return false;
+            }
+        });
+
+        return true;
+    }
 
 }
